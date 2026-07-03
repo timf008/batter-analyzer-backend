2ce002aa4332246f2daca30f1740df9b852e3252
Tim <93391156+timf008@users.noreply.github.com>
2026-07-02 20:25:56 -0400

    Update leaders.r

--- leaders.r
+++ leaders.r
@@ -12,11 +12,11 @@ df <- read.csv(file_path, stringsAsFactors = FALSE)
 
 df <- df %>%
   mutate(
+    Kpct  = round((SO / PA) * 100, 1),
+    BBpct = round((BB / PA) * 100, 1),
     BA    = round(BA, 3),
     OBP   = round(OBP, 3),
-    SLG   = round(SLG, 3),
-    Kpct  = round(Kpct, 1),
-    BBpct = round(BBpct, 1)
+    SLG   = round(SLG, 3)
   )
 
 cat(toJSON(df, pretty = FALSE, auto_unbox = TRUE))
